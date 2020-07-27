27dd239ce9266c134a3246d06b5f544beee15688
Irron Williams <irron.williams@gmail.com>
2020-07-27 14:17:27 -0700

    checking in gitignore and envsample

---
+++ functions/getHighScores.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+
+const Airtable = require('airtable')
+
+Airtable.configure({
+    apiKey: process.env.AIRTABLE_API_KEY, //referencing environment variable from .env 
+})
+
+const base = Airtable.base(process.env.AIRTABLE_BASE) 
+const table = base.table(process.env.AIRTABLE_TABLE)  
+
+exports.handler = async (event) => { 
+    try {
+        const records = await table.select({}).firstPage() //this calls a promise, needs to await response
+        const formattedRecords = records.map((record) => ({
+            id: record.id,
+            fields: record.fields,
+        }))
+        return {
+            statusCode: 200,
+            body: JSON.stringify(formattedRecords) //returns an array
+        } 
+    } catch(err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({err: "Failed to query records in AirTable"})
+        }
+    }
+    
+}
+
+
+/**  This code brings in all the properties from the table 
+ const Airtable = require('airtable')
+
+Airtable.configure({
+    apiKey: 'keyKsO9XgnEfk7rzI'  //setting up API Key
+})
+
+const base = Airtable.base('appA0cyJpKzudltJJ') //creating reference to a base
+const table = base.table('Table1')  //creates a reference to Table1
+
+exports.handler = async (event) => { 
+    const records = await table.select({}).firstPage() //this calls a promise, needs to await response
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(records) //returns an array
+    } 
+}
+ 
+  
+  
+ */
--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => { //only using event
     return {
         statusCode: 200,
         body: JSON.stringify({
-            msg: 'Hello World',
+            msg: 'Hello Worlds',
         }),
     }
 
