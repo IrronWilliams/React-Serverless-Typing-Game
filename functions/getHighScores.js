35d7ddc8df69c1351f7573ed8f2434236b113d3a
IrronWilliams <irron.williams@gmail.com>
2020-12-12 22:39:41 -0800

    Update getHighScores.js

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -1,4 +1,6 @@
-//destructure the table and require in the file 
+/*destructure the table and require in the file. instead of calling the table direclty, importing getHighScores. 
+passing true for these records because want to filter out records that do not have data in them and score >0. 
+*/
 const {table, getHighScores} = require('./utils/airtable')
 
 exports.handler = async (event) => {
